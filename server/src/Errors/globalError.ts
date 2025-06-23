54f30d64d28a00cd87e67e5b82857d07c7ba8262
SaimonNeupane <Saimonbro00007@gmail.com>
2025-06-23 16:09:27 +0545

    Add error handler on index.ts

--- server/src/Errors/globalError.ts
+++ server/src/Errors/globalError.ts
@@ -1,9 +1,8 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import HttpError from './httpError'
 
-const GlobalError: any = (error: HttpError, req: Request, res: Response) => {
-    if (error.statusCode >= 400 && error.statusCode < 500) {
-        
+const GlobalError: any = (error: HttpError, req: Request, res: Response,next:NextFunction) => {
+   
         return res.status(error.statusCode || 500).json({
             status:error.status|| 'Error',
             statusCode:error.statusCode || 500,
@@ -11,15 +10,7 @@ const GlobalError: any = (error: HttpError, req: Request, res: Response) => {
         })
            
         
-    }
-    if(error.statusCode >=500){
-        return res.status(error.statusCode || 500).json({
-            status:error.status || 'Error',
-            statusCode:error.statusCode || 500,
-            message:error.message
-        })
-    }
-
+  
 }
 
 export default GlobalError
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
+import HttpError from "./Errors/httpError";
+import GlobalError from "./Errors/globalError";
+
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +9,15 @@ const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server is running");
 });
+app.use((req:Request,res:Response,next:NextFunction):any=>{
+  return res.status(400).json({
+    status:"Fail",
+    statusCode:404,
+    Message:`The end-Ppint ${req.originalUrl} doesnot exists`
+  })
+})
+
+app.use(GlobalError)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
