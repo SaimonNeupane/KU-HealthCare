0be67822894c5da2bc707611038f986b27cafdc4
SaimonNeupane <Saimonbro00007@gmail.com>
2025-06-23 17:25:33 +0545

    add basic login API for users

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,19 @@
 import express, { Express, NextFunction, Request, Response } from "express";
 import HttpError from "./Errors/httpError";
 import GlobalError from "./Errors/globalError";
+import LoginRoutes from './routes/loginRoute'
+import bodyParser, { json } from "body-parser";
 
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.json())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server is running");
 });
+app.use(`/user`,LoginRoutes)
+
 app.use((req:Request,res:Response,next:NextFunction):any=>{
   return res.status(400).json({
     status:"Fail",
