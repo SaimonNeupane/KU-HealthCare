fc4be0e57dfdeac47ac27bb6a5cbe623524978c3
Keshav Raj Sharma <jdkeshav01@gmail.com>
2025-07-05 13:01:49 +0545

    ADD doctor MyPatients and Notification pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import {  Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -6,24 +6,26 @@ import NewPatient from "./pages/Receptionist/NewPatient.tsx";
 import Dashboard from "./pages/Receptionist/DashBoard.tsx";
 import Notification from "./pages/Doctor/Notification.tsx";
 import AdminPage from "./pages/Admin/AdminPage.tsx";
+import MyPatients from "./pages/Doctor/MyPatients.tsx";
 
 function App() {
   return (
-      <Routes>
-        <Route element={<MainLayout />}></Route>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        
-        <Route path="/receptionist/newpatient" element={<NewPatient />} />
-        <Route path="/receptionist" element={<Navigate to='/receptionist/dashboard'/>} />
-        <Route path="/receptionist/dashboard" element={<Dashboard />} />
-        <Route path="/doctor" element={<Notification/>}/>
-        
-        {/* Fix this line - remove the /* */}
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/admin/*" element={<AdminPage />} />
-      </Routes>
+    <Routes>
+      <Route element={<MainLayout />}></Route>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route path="/receptionist/newpatient" element={<NewPatient />} />
+      <Route path="/receptionist" element={<Navigate to='/receptionist/dashboard' />} />
+      <Route path="/receptionist/dashboard" element={<Dashboard />} />
+      <Route path="/doctorNotification" element={<Notification />} />
+      <Route path="/doctorMyPatients" element={<MyPatients />} />
+
+      {/* Fix this line - remove the /* */}
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/admin/*" element={<AdminPage />} />
+    </Routes>
   );
 }
 
