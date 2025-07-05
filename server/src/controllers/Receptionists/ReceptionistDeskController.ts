f89e7571bee710f569b13847dd8d19f70c96c3af
SaimonNeupane <Saimonbro00007@gmail.com>
2025-07-05 15:03:46 +0545

    Add Pateint Details Branch

--- server/src/controllers/Receptionists/ReceptionistDeskController.ts
+++ server/src/controllers/Receptionists/ReceptionistDeskController.ts
@@ -1,23 +1,81 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 import AsyncError from "../../Errors/asyncError";
 import HttpError from "../../Errors/httpError";
 
+const client = new PrismaClient();
 
-const client= new PrismaClient();
+export const DoctorDetails = AsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const doctors = await client.user.findMany({
+      where: {
+        role: 'doctor',
+        doctor:{
+            isNot:null
+        }
+      },
+     select:{
+        email:true,
+        doctor:{
+            select:{
+                first_name:true,
+                last_name:true,
+                workingFrom:true,
+                workingTo:true,
+                department:{
+                    select:{
+                        name:true
+                    }
+                },
+                is_online:true,
+                doctor_id:true
 
-export const DoctorDetails=AsyncError(async(req:Request,res:Response,next:NextFunction)=>{
-    const doctors=await client.doctor.findMany({
+            }
+        }
+     }
+    });
+    if (doctors.length==0) {
+      next(new HttpError(404, "no doctors found"));
+    }
+    return res.status(200).json({
+      status: "success",
+      statusCode: 200,
+      doctors,
+    });
+  }
+);
+
+export const PatientDetials=AsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    const patients=await client.patient.findMany({
         select:{
+            first_name:true,
+            last_name:true,
+            Appointment:{
+                select:{
+                    status:true,
+                    LabTest:{
+                        select:{
+                            status:true
+                        }
+                    }
+                }
+            },
+            bed:{
+                select:{
+                    bed_number:true
+                }
+            }
             
+
+
         }
     })
-    if(!doctors){
-        next(new HttpError(404,'no doctors found'))
+    if(patients.length==0){
+        next(new HttpError(404,'no patients found'))
     }
     return res.status(200).json({
         status:'success',
         statusCode:200,
-        doctors
+        patients
     })
 })
--- server/src/routes/Receptionists/receptionistRoutes.ts
+++ server/src/routes/Receptionists/receptionistRoutes.ts
@@ -1,7 +1,7 @@
 import { Router} from "express";
-import { DoctorDetails } from "../../controllers/Receptionists/ReceptionistDeskController";
+import { DoctorDetails,PatientDetials } from "../../controllers/Receptionists/ReceptionistDeskController";
 const router:Router= Router();
 
 router.get('/dashboard',DoctorDetails)
-
+router.get('/patient',PatientDetials)
 export default router;
