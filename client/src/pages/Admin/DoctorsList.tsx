0970634ef03913f7f6618c1cfe8f2da573ef0e36
ParikchitSen <parikchitsen77@gmail.com>
2025-08-07 09:53:01 +0545

    Refactored doctor list ui in Admin

--- client/src/pages/Admin/DoctorsList.tsx
+++ client/src/pages/Admin/DoctorsList.tsx
@@ -252,7 +252,7 @@ export default function DoctorsList() {
               <tr key={doctor.doctor_id} className="hover:bg-gray-50">
                 <td className="px-6 py-4 whitespace-nowrap">
                   <div className="text-sm font-medium text-gray-900">
-                    {doctor.first_name} {doctor.last_name}
+                    Dr. {doctor.first_name} {doctor.last_name}
                   </div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
